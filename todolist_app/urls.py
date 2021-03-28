302e9dff84f7420fafb1b9d006c4adb25e9a5cc3
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2021-03-29 00:00:57 +0100

    edit

--- todolist_app/urls.py
+++ todolist_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.todolist, name='todolist'),
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
+    path('delete/<task_id>', views.delete_task, name='delete_task'),
+    path('edit/<task_id>', views.edit_task, name='edit_task'),
+    path('complate/<task_id>', views.complate_task, name='complate_task'),
+    path('pending/<task_id>', views.pending_task, name='pending_task'),
 ]
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Tasklist
 from .forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 # Create your views here.
 
 def todolist(request):
@@ -11,12 +12,54 @@ def todolist(request):
         form = TaskForm(request.POST or None)
         if form.is_valid():
             form.save()
-        messages.success(request, ("New Task Added!"))
+        messages.success(request, 'New Task Added!')
         return redirect('todolist')
     else:
-        all_tasks = Tasklist.objects.all
-
+        all_tasks = Tasklist.objects.all()
+        paginator = Paginator(all_tasks, 5)
+        page = request.GET.get('pg')
+        all_tasks = paginator.get_page(page)
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
+
+def delete_task(request, task_id):
+    task = Tasklist.objects.get(pk=task_id)
+    task.delete()
+    return redirect('todolist')
+
+def edit_task(request, task_id):
+
+    if request.method == "POST":
+        task = Tasklist.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance=task)
+        if form.is_valid():
+            form.save()
+        messages.success(request, 'Task Edited!')
+        return redirect('todolist')
+    else:
+        task_obj = Tasklist.objects.get(pk=task_id)
+
+        return render(request, 'edit.html', {'task_obj': task_obj})
+
+def complate_task(request, task_id):
+    task = Tasklist.objects.get(pk=task_id)
+    # print('task = ', task)
+    # print('---------------------------------')
+    # print('task.done = ', task)
+    task.done = True
+    task.save()
+    return redirect('todolist')
+
+def pending_task(request, task_id):
+    task = Tasklist.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    return redirect('todolist')
+
+def index(request):
+    context = {
+        'index_text': 'welcome index page.'
+    }
+    return render(request, 'index.html', context)
 
 def contact(request):
     context = {
