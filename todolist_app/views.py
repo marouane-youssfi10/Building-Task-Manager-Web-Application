f2aa7be377ceca1b2c5ee0b1558c45a99d864b21
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2021-03-28 17:46:34 +0200

    edit

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tasklist
 
 # Create your views here.
 
 def todolist(request):
-    context = {
-        'Welcome_text': 'welcome todo list page.'
-    }
-    return render(request, 'todolist.html', context)
+
+    all_tasks = Tasklist.objects.all
+
+    return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 def contact(request):
     context = {
-        'Welcome_text': 'welcome contact page.'
+        'contact_text': 'welcome contact page.'
     }
-    return render(request, 'todolist.html', context)
+    return render(request, 'contact.html', context)
 
 def about(request):
     context = {
-        'Welcome_text': 'welcome About page.'
+        'about_text': 'welcome About page.'
     }
-    return render(request, 'todolist.html', context)
+    return render(request, 'about.html', context)
