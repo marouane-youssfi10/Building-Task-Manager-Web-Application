ac73acd2be62c34256ad877680c8df232224b75c
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2021-03-29 13:22:38 +0100

    adding users_app

--- users_app/views.py
+++ users_app/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomRegisterForm
+from django.contrib import messages
 
 def register(request):
-    # return render(request, 'register.html')
-    return HttpResponse('users_app working!')
+    if request.method == 'POST':
+        register_form = CustomRegisterForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'New User Account Created, Login To Get Started!')
+            return redirect('register')
+    else:
+        register_form = CustomRegisterForm()
+
+    return render(request, 'register.html', {'register_form': register_form})
